dcc21ee740b9b5ce59f60c838b2b02f3fc81d3c0
massimorenson <massimo.renson@student.howest.be>
2017-10-26 10:06:49 +0200

    fix3upload

--- upload.php
+++ upload.php
@@ -47,7 +47,8 @@ if(isset($_POST['submit']))
                 $fileDestenation = 'uploads/'.$fileNameNew; // waar de file moet worden upgeload
                 move_uploaded_file($fileTmpName,$fileDestenation); // de file wordt nu geupload naar waar hij moet
                 // de tijdelijke plaats van de file wordt naar de eindbestemming gebracht
-                header("Location: profile.php?uploadsucces");
+                header("Location: profile.php?uploadsucces",true,302);
+                exit;
             }
             else
             {
