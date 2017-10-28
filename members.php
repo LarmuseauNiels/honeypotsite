b433abea41b0906e51c26608759d05dee4b73a65
Niels Larmuseau <nielslarmuseau@gmail.com>
2017-10-28 17:12:31 +0200

    members link to their respective profiles

--- members.php
+++ members.php
@@ -10,14 +10,15 @@ $users = $db->getUsers();
 echo('<p><span class="badge">'.count($users).'</span> Members:</p><br>');
 echo('<div class="row">');
 foreach ($users as $user){
+  $userurl = 'profile.php?id='.$user->userid;
   echo ('<div>');
   echo ('<div class="col-sm-2 text-center">');
-  echo ('<a href="'.'http://example.com'.'">');
+  echo ('<a href="'.$userurl.'">');
   echo ('<img src="');
   if ($user->filepath !== null){echo ($user->filepath);}
   else{echo 'assets/images/default-user-image.png';}
   echo('" class="img-circle" height="65" width="65" alt="Avatar"></a></div><div class="col-sm-10"><br><h4>');
-  echo ('<a href="'.'http://example.com'.'">');
+  echo ('<a href="'.$userurl.'">');
   echo ($user->username);
   echo(' </a>');
 if($authenticator->getRole() === "A"){echo('<small>remove</small>');}
