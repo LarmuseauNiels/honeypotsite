429be21cf2df89a7011ac46a6c13821212b1ffca
Niels Larmuseau <nielslarmuseau@gmail.com>
2017-11-02 14:35:41 +0100

    making the chat work

--- chat.php
+++ chat.php
@@ -3,55 +3,53 @@ session_start();
 require_once 'autoloader.php';
 output::htmlheader();
 $authenticator = new auth();
+$userid=$authenticator->getUserid();
+$db = dbrepo::getdbinstance();
 output::navigation("chat",$authenticator->getLogedin(),$authenticator->getRole());
 
-?>
-<h4>Post a Message:</h4>
-      <form role="form">
-        <div class="form-group">
-          <textarea class="form-control" rows="3" required></textarea>
-        </div>
-        <button type="submit" class="btn btn-success">Submit</button>
-      </form>
-      <br><br>
-      <p><span class="badge">3</span> Messages:</p><br>      
-      <div class="row">
-        <div>
-        <div class="col-sm-2 text-center">
-          <img src="https://i.imgur.com/0W49c50.png" class="img-circle" height="65" width="65" alt="Avatar">
-        </div>
-        <div class="col-sm-10">
-          <h4>Niels <small>Sep 29, 2017, 9:12 PM</small></h4>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-          <br>
-        </div>
-        </div>
-        <div class="col-sm-2 text-center">
-          <img src="https://i.imgur.com/qWoHxMv.jpg" class="img-circle" height="65" width="65" alt="Avatar">
-        </div>
-        <div class="col-sm-10">
-          <h4>John <small>Sep 25, 2017, 8:25 PM</small></h4>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </p>
-          <br>
-        </div>
-        <div class="col-sm-2 text-center">
-          <img src="https://i.imgur.com/qWoHxMv.jpg" class="img-circle" height="65" width="65" alt="Avatar">
-        </div>
-        <div class="col-sm-10">
-          <h4>John 2 <small>Sep 25, 2017, 8:25 PM</small></h4>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </p>
-          <br>
-        </div>
-        
-      </div>
-<?php
+if($authenticator->getLogedin()){
+  showPostaMessage();
+  if(isset($_POST['submit']))
+  {
+    $message=$_POST['message'];
+    $db->addMessage($userid,$message);
+    header("Location: chat.php");
+  }
+}
+$messages = $db->getMessages();
+toonMessages($messages,$db);
 
+function showPostaMessage()
+{
+  ?>
+  <h4>Post a Message:</h4>
+  <form role="form" method="POST" action="chat.php">
+    <div class="form-group">
+      <textarea name="message" class="form-control" rows="3" required></textarea>
+    </div>
+    <button type="submit" name="submit" class="btn btn-success">Submit</button>
+  </form>
+  <br><br>
+  <?php
+}
+
+function toonMessages($messages,$db)
+{
+  echo "<p><span class='badge'>".sizeof($messages)."</span> Messages:</p><br> ";
+  echo "<div class='row'>";
+    foreach($messages as $message){
+      $profilePictureObj=$db->getPictureForUser($message->userid);
+      $userObj=$db->getUserFromID($message->userid);
+      echo "  <div class='col-sm-2 text-center'>";
+      echo "    <img src='".$profilePictureObj->filepath."' class='img-circle' height='65' width='65' alt='Avatar'>";
+      echo "  </div>";
+      echo "  <div class='col-sm-10'>";
+      echo "    <h4>".$userObj->username."<small>  ".$message->timestamp."</small></h4>";
+      echo "    <p>".$message->message."</p>";
+      echo "    <br>";
+      echo "  </div>";
+    }
+  echo "</div>";
+}
 output::pageend();
 
