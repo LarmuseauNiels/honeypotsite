81bb1284e58d0c65f1fd78174cdce0c3ec8f483b
Niels Larmuseau <nielslarmuseau@gmail.com>
2017-11-16 11:28:16 +0100

    feedback captcha

--- feedbackSubmit.php
+++ feedbackSubmit.php
@@ -8,10 +8,14 @@ if(!$authenticator->getLogedin()){header("Location: login.php");}
 else{
 if(isset($_POST['submit']))
 {
+  if(captcha::checkresponce($response)){    
   $userid=$authenticator->getUserid();
   $message= $_POST['feedback'];	
   $db->addFeedback($userid, $message);
   header("Location: feadback.php?feedbackSucces");
+  $response = $_POST["g-recaptcha-response"];
   
+}
+  else{ header("Location: feadback.php?feedbackFail");}
 }
 }
